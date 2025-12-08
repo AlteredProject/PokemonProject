54fceec09fd9a8628d83b98c31d1e2c20b555d8d
AlteredProject <125555532+AlteredProject@users.noreply.github.com>
2025-12-08 10:54:46 +0100

    AssetSetter to place objects in world

---
+++ src/main/AssetSetter.java
@@ -0,0 +1,21 @@
+package main;
+
+import object.OBJ_Pokeball;
+import object.OBJ_Rock;
+
+public class AssetSetter {
+    GamePanel gp;
+    public AssetSetter(GamePanel gp) {
+        this.gp = gp;
+    }
+
+    public void setObject() {
+            //gp.obj[0] = new OBJ_Pokeball();
+            //gp.obj[0].worldX = 9 * gp.tileSize;
+            //gp.obj[0].worldY = 4 * gp.tileSize;
+
+            //gp.obj[1] = new OBJ_Rock();
+            //gp.obj[1].worldX = 10 * gp.tileSize;
+            //gp.obj[1].worldY = 4 * gp.tileSize;
+    }
+}
